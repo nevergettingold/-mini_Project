fe989d6031df00a82fba213f1ebdab5d05d3184b
Livelyoneweek <wlvvwhs@naver.com>
2022-02-11 18:19:43 +0900

    최병재
    inint

---
+++ Choi/src/test/java/sparta/miniproject/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.leeyongman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeyongmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
