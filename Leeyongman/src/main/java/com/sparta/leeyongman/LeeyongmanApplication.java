8d26ef042e400d93860b4f4f6fe55b06244f2873
yongman Lee <dydaks7878@gmail.com>
2022-02-11 17:42:42 +0900

    이용만

---
+++ Leeyongman/src/main/java/com/sparta/leeyongman/LeeyongmanApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.leeyongman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeyongmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeyongmanApplication.class, args);
+    }
+
+}
