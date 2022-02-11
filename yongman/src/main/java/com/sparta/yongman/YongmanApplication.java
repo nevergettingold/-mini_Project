b17db873ea03196a2a23a45530eab6ac69f51a48
yongman Lee <dydaks7878@gmail.com>
2022-02-11 18:49:27 +0900

    이용만

---
+++ yongman/src/main/java/com/sparta/yongman/YongmanApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.yongman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YongmanApplication.class, args);
+    }
+
+}
