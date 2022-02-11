9ab982f2efd370a8e8ddfef9d5b3bfb566ff76e7
yongman Lee <dydaks7878@gmail.com>
2022-02-11 19:00:58 +0900

    이용만

---
+++ yongman/src/test/java/com/sparta/yongman/YongmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.yongman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
